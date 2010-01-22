ac0ea5ae0648b79afda30064e6f2e08870d31ddd
peter.stubbs <peter.stubbs@1ab.ltd.uk>
2010-01-22 18:29:52 +0000

    merged in issue 10 code

--- tables/portfolios.sql
+++ tables/portfolios.sql
@@ -4,10 +4,12 @@
 CREATE TABLE portfolios (
     pfid serial not null,
     name character varying(100) NOT NULL,
+    exch character varying(6) NOT NULL,
     uid integer not null,
     parcel numeric(9,2),
     start_date date not null,
-    working_date date not null
+    working_date date not null,
+    unique (uid, exch, name)
 );
 ALTER TABLE public.portfolios OWNER TO postgres;
 ALTER TABLE ONLY portfolios ADD CONSTRAINT portfolios_pkey PRIMARY KEY (pfid);
