f913a10990a033499b8f86fe5a592326290eeaeb
peter.stubbs@1ab.ltd.uk <peter.stubbs@1ab.ltd.uk@20023dc0-f96f-11de-9e29-7d1cd92887ef>
2010-02-01 20:48:47 +0000

    for issue 29

---
+++ branches/trader_0.01_i29/tables/stocks.sql
@@ -0,0 +1,12 @@
+--
+-- Name: stocks; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
+CREATE TABLE stocks (
+    symb character varying(10) NOT NULL,
+    name character varying(100) NOT NULL,
+    exch character varying(6) NOT NULL,
+    first_quote date,
+    last_quote date
+);
+ALTER TABLE public.stocks OWNER TO postgres;
+ALTER TABLE ONLY stocks ADD CONSTRAINT stocks_pkey PRIMARY KEY (symb, exch);
