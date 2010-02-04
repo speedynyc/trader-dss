43d772ea16a67e5329ebbde9db8408d2e2cc01cc
peter.stubbs@1ab.ltd.uk <peter.stubbs@1ab.ltd.uk@20023dc0-f96f-11de-9e29-7d1cd92887ef>
2010-02-04 20:32:57 +0000

    tidying up old branches

--- branches/trader_0.01_i15_digger/views/view_stock_sector.sql
+++
@@ -1,6 +0,0 @@
---
--- Name: view_stock_sector; Type: VIEW; Schema: public; Owner: postgres
---
-CREATE VIEW view_stock_sector AS
-    SELECT stock_sector.symb, stocks.name AS stock_name, stock_sector.sector_id, sectors.name AS sector_name FROM stock_sector, stocks, sectors WHERE (((stock_sector.symb)::text = (stocks.symb)::text) AND (stock_sector.sector_id = sectors.sector_id));
-ALTER TABLE public.view_stock_sector OWNER TO postgres;
