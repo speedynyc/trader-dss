8a1c45ee83d2af01e9eb5b0d5b973418c782dd67
peter.stubbs@1ab.ltd.uk <peter.stubbs@1ab.ltd.uk@20023dc0-f96f-11de-9e29-7d1cd92887ef>
2010-02-04 20:30:50 +0000

    tidying up earliest branches

--- branches/trader_0.01_i1_digger/tables/trade_dates.sql
+++
@@ -1,9 +0,0 @@
---
--- Name: trade_dates; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
---
-CREATE TABLE trade_dates (
-    date date,
-    exch character varying(6)
-);
-ALTER TABLE public.trade_dates OWNER TO postgres;
-CREATE INDEX idx_date_exch ON trade_dates USING btree (date, exch);
