64633172be68dab514a585522fb1c45dcc341575
peter.stubbs@1ab.ltd.uk <peter.stubbs@1ab.ltd.uk@20023dc0-f96f-11de-9e29-7d1cd92887ef>
2010-02-09 16:59:39 +0000

    merge issue 16 back to trunk

--- trunk/html/booty.php
+++ trunk/html/booty.php
@@ -10,8 +10,9 @@ $uid = $_SESSION['uid'];
 $pf_id = $_SESSION['pfid'];
 $pf_name = get_pf_name($pf_id);
 $pf_working_date = get_pf_working_date($pf_id);
-$pf_exch = get_pf_exch($pf_id);
-$next_trade_day = next_trade_day($pf_working_date, $pf_exch);
+$exch = new exchange(get_pf_exch($pf_id));
+$pf_exch = $exch->getExch();
+$next_trade_day = $exch->next_trade_day($pf_working_date);
 
 try {
     $pdo = new PDO("pgsql:host=$db_hostname;dbname=$db_database", $db_user, $db_password);
@@ -20,7 +21,7 @@ try {
     die("ERROR: Cannot connect: " . $e->getMessage());
 }
 
-function draw_table($pf_id, $pf_working_date, $pf_exch, $pf_name)
+function draw_performance_table($pf_id, $pf_working_date, $exch, $pf_name)
 {
     global $pdo, $next_trade_day, $pf_name;
     $pf_opening_balance = get_pf_opening_balance($pf_id);
@@ -29,7 +30,8 @@ function draw_table($pf_id, $pf_working_date, $pf_exch, $pf_name)
     $pf_cash_in_hand = get_pf_cash_in_hand($pf_id);
     $pf_holdings = get_pf_holdings($pf_id);
     $pf_total = sprintf("%.2f", $pf_cash_in_hand + $pf_holdings);
-    $pf_exchange_name = get_exch_name($pf_exch);
+    $pf_exchange_name = $exch->getName();
+    $pf_exch = $exch->getExch();
     print '<form action="' . $_SERVER['REQUEST_URI'] . '" method="post" name="cart" id="cart">';
     print '<table border="1" cellpadding="5" cellspacing="0" align="center">';
     print '<tr><td>Symb</td><td>Name</td><td>Comment</td><td>Date</td><td>Volume</td><td>Buy Price</td><td>close</td><td>gain</td><td>Value</td>';
@@ -189,4 +191,4 @@ elseif(isset($_POST['sell']))
     }
 }
 
-draw_table($pf_id, $pf_working_date, $pf_exch, $pf_name);
+draw_performance_table($pf_id, $pf_working_date, $exch, $pf_name);
---
+++ trunk/html/docs.php
@@ -0,0 +1,46 @@
+<?php
+include("trader-functions.php");
+redirect_login_pf();
+draw_trader_header('docs');
+// Load the HTML_QuickForm module
+global $db_hostname, $db_database, $db_user, $db_password;
+
+$username = $_SESSION['username'];
+$uid = $_SESSION['uid'];
+$pf_id = $_SESSION['pfid'];
+
+function describe_table($table)
+{
+    global $db_hostname, $db_database, $db_user, $db_password;
+    $col_header_colour = '#3BB9FF';
+    $header_colour = '#357EC7';
+    try {
+        $pdo = new PDO("pgsql:host=$db_hostname;dbname=$db_database", $db_user, $db_password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die("ERROR: Cannot connect: " . $e->getMessage());
+    }
+    $query = "select table_name, column_name, data_type, comment from view_table_details where table_name = '$table' order by column_name";
+    print "<table border=\"1\" cellpadding=\"5\" cellspacing=\"0\" ><tr><td bgcolor=\"$header_colour\" colspan=\"20\" align=\"center\">$table</td></tr>\n";
+    print "<tr><td bgcolor=\"$col_header_colour\">Column Name</td><td bgcolor=\"$col_header_colour\">Data Type</td><td bgcolor=\"$col_header_colour\">Description</td></tr>\n";
+    foreach ($pdo->query($query) as $row)
+    {
+        $column = $row['column_name'];
+        $data_type = $row['data_type'];
+        if ($row['comment'] == '')
+        {
+            $comment = $column;
+        }
+        else
+        {
+            $comment = $row['comment'];
+        }
+        print "<tr><td>$column</td><td>$data_type</td><td>$comment</td></tr>\n";
+    }
+    print "</table>";
+}
+
+describe_table('simple_moving_averages');
+describe_table('standard_deviations_from_mean');
+?>
+
