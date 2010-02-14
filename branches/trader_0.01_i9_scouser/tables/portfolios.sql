473b6679f604d9b1548bb725826efd61dbc84645
b.dalling@locp.co.uk <b.dalling@locp.co.uk@20023dc0-f96f-11de-9e29-7d1cd92887ef>
2010-02-14 13:17:54 +0000

    Rebased the trader_0.01_i9_scouser branch with the trunk

--- branches/trader_0.01_i9_scouser/tables/portfolios.sql
+++ branches/trader_0.01_i9_scouser/tables/portfolios.sql
@@ -2,12 +2,16 @@
 -- Name: portfolios; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
 CREATE TABLE portfolios (
-    pfid integer not null DEFAULT nextval(('sequence_portfolios'::text)::regclass),
+    pfid serial not null,
     name character varying(100) NOT NULL,
+    exch character varying(6) NOT NULL,
     uid integer not null,
-    parcel numeric(9,2),
-    start_date date not null,
-    working_date date not null
+    parcel numeric(12,2),
+    working_date date not null,
+    hide_names char(1),
+    sell_stop smallint,
+    auto_sell_stop char(1),
+    unique (uid, exch, name)
 );
 ALTER TABLE public.portfolios OWNER TO postgres;
 ALTER TABLE ONLY portfolios ADD CONSTRAINT portfolios_pkey PRIMARY KEY (pfid);
