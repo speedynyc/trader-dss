4dcfcdf1872eaf9ebc39f953b57e053f629e6c73
peter.stubbs@1ab.ltd.uk <peter.stubbs@1ab.ltd.uk@20023dc0-f96f-11de-9e29-7d1cd92887ef>
2010-03-22 17:08:34 +0000

    added sanity contstraints on the quotes table

--- trunk/constraints/quotes.sql
+++ trunk/constraints/quotes.sql
@@ -1,1 +1,3 @@
 ALTER TABLE ONLY quotes ADD CONSTRAINT constraint_stocks FOREIGN KEY (symb, exch) REFERENCES stocks(symb, exch) MATCH FULL;
+ALTER TABLE ONLY quotes ADD CONSTRAINT stocks_constraint_low_gt_high check (low <= high);
+ALTER TABLE ONLY quotes ADD CONSTRAINT stocks_volume_gt_zero check (volume > 0);
