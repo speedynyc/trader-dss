5cc66b4237f25ec569a2cd47a1081a49efc80374
peter.stubbs <peter.stubbs@1ab.ltd.uk>
2010-03-29 12:55:17 +0000

    minor correction

--- tables/trades.sql
+++ tables/trades.sql
@@ -4,10 +4,12 @@
 CREATE TABLE trades (
     trid serial not null,
     pfid integer not null,
+    hid integer not null,
     symb character varying(10) NOT NULL,
     date date not null,
     price numeric(9,2) NOT NULL,
     volume numeric(12) NOT NULL,
+    tr_type char(1) NOT NULL,
     comment text;
 );
 ALTER TABLE public.trades OWNER TO postgres;
