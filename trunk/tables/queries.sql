8383d9d099a608061e532e0a443fb1bc8bb00351
peter.stubbs@1ab.ltd.uk <peter.stubbs@1ab.ltd.uk@20023dc0-f96f-11de-9e29-7d1cd92887ef>
2010-03-30 21:41:06 +0000

    minor correction

--- trunk/tables/queries.sql
+++ trunk/tables/queries.sql
@@ -1,19 +1,25 @@
 --
 -- Name: queries; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
 --
-CREATE TABLE queries (
-    qid serial not null,
-    uid integer not null,
-    name character varying(100) unique NOT NULL,
-    sql_select text,
-    sql_from text,
-    sql_where text,
-    sql_order text,
-    sql_order_dir char(4),
-    sql_limit integer,
-    chart_period integer,
-    active boolean,
-    unique (uid, name)
-);
+--
+-- Name: queries; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
+CREATE TABLE queries
+(
+ qid serial NOT NULL,
+ uid integer NOT NULL,
+ "name" character varying(100) NOT NULL,
+ sql_select text,
+ sql_from text,
+ sql_where text,
+ sql_order text,
+ sql_order_dir character(4),
+ sql_limit integer,
+ chart_period integer,
+ active boolean,
+ CONSTRAINT queries_pkey PRIMARY KEY (qid),
+ CONSTRAINT queries_uid FOREIGN KEY (uid)
+ REFERENCES users (uid) MATCH FULL
+ ON UPDATE NO ACTION ON DELETE NO ACTION
+ );
 ALTER TABLE public.queries OWNER TO postgres;
-ALTER TABLE ONLY queries ADD CONSTRAINT queries_pkey PRIMARY KEY qid;
