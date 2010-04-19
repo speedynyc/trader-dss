72f4f1bf4406a5402e9d5f495df2a87842756066
peter.stubbs@1ab.ltd.uk <peter.stubbs@1ab.ltd.uk@20023dc0-f96f-11de-9e29-7d1cd92887ef>
2010-04-19 20:28:23 +0000

    added the core trader classes to a helper file

--- branches/trader_0.01_i64/trader-DSS/views/portfolios_view.php
+++ branches/trader_0.01_i64/trader-DSS/views/portfolios_view.php
@@ -13,20 +13,19 @@
         update_select_portfolios();
     });
 
+    /*
     function update_select_portfolios(responseText, statusText, xhr, $form)
     {
         $('#select_portfolio').load('/trader/get_portfolios');
     }
+    */
 </script>
 <table border="1" cellpadding="5" cellspacing="0" align="center"><tr><td>
 <div id="portfolio_tabs">
     <ul>
-        <li><a href="#select_portfolio"><span>Select Portfolio</span></a></li>
+        <li><a href="/trader/get_portfolios"><span>Select Portfolio</span></a></li>
         <li><a href="#create_portfolio"><span>Create Portfolio</span></a></li>
     </ul>
-    <div id="select_portfolio">
-        Select the portfolio here
-    </div>
     <div id="create_portfolio">
         Create a portfolio here
     </div>
