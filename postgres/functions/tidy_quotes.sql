8254f45ecc8bbabc398518fb2f1152a87f7af33f
peter.stubbs <peter.stubbs@1ab.ltd.uk>
2010-12-19 18:39:35 +0000

    updates

--- postgres/functions/tidy_quotes.sql
+++ postgres/functions/tidy_quotes.sql
@@ -1,19 +1,18 @@
---
--- Name: tidy_quotes(character varying, character varying); Type: FUNCTION; Schema: public; Owner: postgres
---
-CREATE or replace FUNCTION tidy_quotes(symbol character varying, exchange character varying) RETURNS integer
-    LANGUAGE plpgsql
-    AS $$
+CREATE OR REPLACE FUNCTION tidy_quotes(symbol character varying, exchange character varying)
+  RETURNS integer AS
+$BODY$
  BEGIN 
  -- delete the symbol from the derived tables
+ delete from gains where symb = symbol and exch = exchange;
+ delete from gaps where symb = symbol and exch = exchange;
+ delete from indicators where symb = symbol and exch = exchange;
+ delete from moving_averages where symb = symbol and exch = exchange;
  delete from standard_deviations_from_mean where symb = symbol and exch = exchange;
  delete from quotes where symb = symbol and exch = exchange;
- delete from stock_dates where symb = symbol and exch = exchange;             
- delete from gains where symb = symbol and exch = exchange;
- delete from moving_averages where symb = symbol and exch = exchange;  
- delete from indicators where symb = symbol and exch = exchange;  
+ delete from stocks where symb = symbol and exch = exchange;
  return 0;
 END;
-$$;
-ALTER FUNCTION public.tidy_quotes(symbol character varying, exchange character varying) OWNER TO postgres;
+$BODY$
+  LANGUAGE 'plpgsql' VOLATILE;
+ALTER FUNCTION tidy_quotes(character varying, character varying) OWNER TO postgres;
 
