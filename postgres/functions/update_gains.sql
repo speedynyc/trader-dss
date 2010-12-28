c3faec38e0feb03411f2630a9022281d98bea8f2
peter.stubbs <peter.stubbs@1ab.ltd.uk>
2010-12-28 16:43:55 +0000

    fixed update_gains.sql to use adjusted close not close

--- postgres/functions/update_gains.sql
+++ postgres/functions/update_gains.sql
@@ -25,7 +25,7 @@ BEGIN
     --
     -- start with the 200 select. If we don't get any records back then the this will be the first record for this symbol
     -- gain200
-    select topN.date, topN.symb, topN.exch, topN.close into gain200 from (select date, symb, exch, close from quotes where symb = new_symb and exch = new_exch and date < new_date order by date desc limit 200) as topN order by date asc limit 1;
+    select topN.date, topN.symb, topN.exch, topN.close into gain200 from (select date, symb, exch, adj_close from quotes where symb = new_symb and exch = new_exch and date < new_date order by date desc limit 200) as topN order by date asc limit 1;
     IF NOT FOUND THEN
         -- no records found, must be the first record. Populate it with the symbol details and zeros and don't bother with the 20,30 etc.
         BEGIN
@@ -41,22 +41,22 @@ BEGIN
             insert into gains (date, symb, exch, d_200, c_200, gain_200) values (new_date, new_symb, new_exch, gain200.date, gain200.close, new_close - gain200.close);
         END IF;
         -- gain100
-        select topN.date, topN.symb, topN.exch, topN.close into gain100 from (select date, symb, exch, close from quotes where symb = new_symb and exch = new_exch and date < new_date order by date desc limit 100) as topN order by date asc limit 1;
+        select topN.date, topN.symb, topN.exch, topN.close into gain100 from (select date, symb, exch, adj_close from quotes where symb = new_symb and exch = new_exch and date < new_date order by date desc limit 100) as topN order by date asc limit 1;
         update gains set d_100 = gain100.date, c_100 = gain100.close, gain_100 = new_close - gain100.close where symb = new_symb and exch = new_exch and date = new_date;
         -- gain50
-        select topN.date, topN.symb, topN.exch, topN.close into gain50 from (select date, symb, exch, close from quotes where symb = new_symb and exch = new_exch and date < new_date order by date desc limit 50) as topN order by date asc limit 1;
+        select topN.date, topN.symb, topN.exch, topN.close into gain50 from (select date, symb, exch, adj_close from quotes where symb = new_symb and exch = new_exch and date < new_date order by date desc limit 50) as topN order by date asc limit 1;
         update gains set d_50 = gain50.date, c_50 = gain50.close, gain_50 = new_close - gain50.close where symb = new_symb and exch = new_exch and date = new_date;
         -- gain30
-        select topN.date, topN.symb, topN.exch, topN.close into gain30 from (select date, symb, exch, close from quotes where symb = new_symb and exch = new_exch and date < new_date order by date desc limit 30) as topN order by date asc limit 1;
+        select topN.date, topN.symb, topN.exch, topN.close into gain30 from (select date, symb, exch, adj_close from quotes where symb = new_symb and exch = new_exch and date < new_date order by date desc limit 30) as topN order by date asc limit 1;
         update gains set d_30 = gain30.date, c_30 = gain30.close, gain_30 = new_close - gain30.close where symb = new_symb and exch = new_exch and date = new_date;
         -- gain20
-        select topN.date, topN.symb, topN.exch, topN.close into gain20 from (select date, symb, exch, close from quotes where symb = new_symb and exch = new_exch and date < new_date order by date desc limit 20) as topN order by date asc limit 1;
+        select topN.date, topN.symb, topN.exch, topN.close into gain20 from (select date, symb, exch, adj_close from quotes where symb = new_symb and exch = new_exch and date < new_date order by date desc limit 20) as topN order by date asc limit 1;
         update gains set d_20 = gain20.date, c_20 = gain20.close, gain_20 = new_close - gain20.close where symb = new_symb and exch = new_exch and date = new_date;
         -- gain10
-        select topN.date, topN.symb, topN.exch, topN.close into gain10 from (select date, symb, exch, close from quotes where symb = new_symb and exch = new_exch and date < new_date order by date desc limit 10) as topN order by date asc limit 1;
+        select topN.date, topN.symb, topN.exch, topN.close into gain10 from (select date, symb, exch, adj_close from quotes where symb = new_symb and exch = new_exch and date < new_date order by date desc limit 10) as topN order by date asc limit 1;
         update gains set d_10 = gain10.date, c_10 = gain10.close, gain_10 = new_close - gain10.close where symb = new_symb and exch = new_exch and date = new_date;
         -- gain1
-        select topN.date, topN.symb, topN.exch, topN.close into gain1 from (select date, symb, exch, close from quotes where symb = new_symb and exch = new_exch and date < new_date order by date desc limit 1) as topN;
+        select topN.date, topN.symb, topN.exch, topN.close into gain1 from (select date, symb, exch, adj_close from quotes where symb = new_symb and exch = new_exch and date < new_date order by date desc limit 1) as topN;
         update gains set gain_1 = new_close - gain1.close where symb = new_symb and exch = new_exch and date = new_date;
     END IF;
     -- Work out the max/min over the same periods
